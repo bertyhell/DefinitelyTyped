f8f005e4861019af912375b17bf25593fc2725eb
Bert Verhelst <verhelstbert@gmail.com>
2016-03-25 11:25:44 +0100

    Added more options and functions + better types

--- iscroll/iscroll-5.d.ts
+++ iscroll/iscroll-5.d.ts
@@ -4,88 +4,90 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface IScrollOptions {
-	//hScroll?: boolean;
-	//vScroll?: boolean;
-	x?: number;
-	y?: number;
-	bounce?: boolean;
-	bounceLock?: boolean;
-	momentum?: boolean;
-	lockDirection?: boolean;
-	useTransform?: boolean;
-	useTransition?: boolean;
-	topOffset?: number;
-	checkDOMChanges?: boolean;
-	handleClick?: boolean;
+  x?: number;
+  y?: number;
+  bounce?: boolean;
+  bounceLock?: boolean;
+  momentum?: boolean;
+  lockDirection?: boolean;
+  useTransform?: boolean;
+  useTransition?: boolean;
+  topOffset?: number;
+  checkDOMChanges?: boolean;
+  handleClick?: boolean;
 
-	// Scrollbar
-	hScrollbar?: boolean;
-	vScrollbar?: boolean;
-	fixedScrollbar?: boolean;
-	hideScrollbar?: boolean;
-	fadeScrollbar?: boolean;
-	scrollbarClass?: string;
+  // Scrollbar
+  hScrollbar?: boolean;
+  vScrollbar?: boolean;
+  fixedScrollbar?: boolean;
+  hideScrollbar?: boolean;
+  fadeScrollbar?: boolean;
+  scrollbarClass?: string;
 
-	// Zoom
-	zoom?: boolean;
-	zoomMin?: number;
-	zoomMax?: number;
-	doubleTapZoom?: number;
-	wheelAction?: string;
+  // Zoom
+  zoom?: boolean;
+  zoomMin?: number;
+  zoomMax?: number;
+  doubleTapZoom?: number;
+  mouseWheel?: boolean;
+  wheelAction?: string;
+  snap?: string | boolean;
+  snapThreshold?: number;
 
-	
-	///String or boolean
-	snap?: any;
-	snapThreshold?: number;
+  // New in IScroll 5?
+  resizeIndicator?: boolean;
+  mouseWheelSpeed?: number;
+  startX?: number;
+  startY?: number;
+  scrollX?: boolean;
+  scrollY?: boolean;
+  scrollbars?: boolean | string;
+  shrinkScrollbars?: string;
+  interactiveScrollbars?: boolean;
+  releaseScroll?: boolean;
+  fadeScrollbars?: boolean;
+  directionLockThreshold?: number;
 
-	//new in IScroll 5?
+  bounceTime?: number;
 
-	resizeIndicator?: boolean;
-	mouseWheelSpeed?: number;
-	startX?: number;
-	startY?: number;
-	scrollX?: boolean;
-	scrollY?: boolean;
-	directionLockThreshold?: number;
+  ///String or function
+  bounceEasing?: any;
 
-	bounceTime?: number;
+  preventDefault?: boolean;
+  preventDefaultException?: boolean;
 
-	///String or function
-	bounceEasing?: any;
-	
-	preventDefault?: boolean;
-	preventDefaultException?: boolean;
+  HWCompositing?: boolean;
 
-	HWCompositing?: boolean;
+  freeScroll?: boolean;
 
-	freeScroll?: boolean;
-
-	resizePolling?: number;
-	tap?: boolean;
-	click?: boolean;
-	invertWheelDirection?: boolean;
-
-	///Boolean or string
-	eventPassthrough?: any;
+  resizePolling?: number;
+  tap?: boolean;
+  click?: boolean;
+  invertWheelDirection?: boolean;
+  eventPassthrough?: string | boolean;
 }
 
 declare class IScroll {
+  constructor (element: string, options?: IScrollOptions);
+  constructor (element: HTMLElement, options?: IScrollOptions);
 
-	constructor (element: string, options?: IScrollOptions);
-	constructor (element: HTMLElement, options?: IScrollOptions);
+  x: number;
+  y: number;
 
-	destroy(): void;
-	refresh(): void;
-	scrollTo(x: number, y: number, time?: number, relative?: boolean): void;
-	scrollToElement(element: string, time?: number): void;
-	scrollToElement(element: HTMLElement, time?: number): void;
-	goToPage(pageX: number, pageY: number, time?: number): void;
-	disable(): void;
-	enable(): void;
-	stop(): void;
-	zoom(x: number, y: number, scale: number, time?: number): void;
-	isReady(): boolean;
+  destroy(): void;
+  refresh(): void;
+  scrollTo(x: number, y: number, time?: number, relative?: boolean): void;
+  scrollToElement(element: string, time?: number): void;
+  scrollToElement(element: HTMLElement, time?: number): void;
+  scrollToElement(element: HTMLElement, time?: number, offsetX?: number | boolean, offsetY?: number | boolean, easing?: Function): void;
+  goToPage(pageX: number, pageY: number, time?: number): void;
+  disable(): void;
+  enable(): void;
+  stop(): void;
+  zoom(x: number, y: number, scale: number, time?: number): void;
+  isReady(): boolean;
 
-	// Events
-	on: (type: string, fn: () => void) => void;
+  // Events
+  on(type: string, fn: (evt?: any) => void): void;
+  off(type: string, fn?: (evt?: any) => void): void;
 }
